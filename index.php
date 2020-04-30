af79827c2c61655d743d2ca5cd54558f5c286850
belikewata <kelvin.truong@gmail.com>
2020-04-30 20:05:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-header("Location: https://www.linkedin.com/in/kelvintruong/?originalSubdomain=hk");
-exit();
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Be Like Water</title>
+</head>
+
+<body>
+<!--p>Don't get set into one form, adapt it and build your own, and let it grow, <strong>be like water</strong>. Empty your mind, be formless, shapeless — like water. Now you put water in a cup, it becomes the cup; You put water into a bottle it becomes the bottle; You put it in a teapot it becomes the teapot. Now water can flow or it can crash. Be water, my friend.</p-->
+<p>Hello Wata!</p>
+</body>
+</html>
